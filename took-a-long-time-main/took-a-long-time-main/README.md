31fe3d423f7158a63926db653d72fa522849a062
hideki <74686890+hidekily@users.noreply.github.com>
2023-11-10 14:04:32 -0300

    Add files via upload

--- took-a-long-time-main/took-a-long-time-main/README.md
+++ took-a-long-time-main/took-a-long-time-main/README.md
@@ -1,2 +1,5 @@
 # took-a-long-time
 pfp's test
+
+
+site do tcc SENAC SMP 2023
---
+++ took-a-long-time-main/took-a-long-time-main/subpages/subpage2/08kkkk.js
@@ -0,0 +1,19 @@
+// function hover(){
+//     document.getElementById("hdk1").style.backgroundColor = 'var(--smooth)'
+//     document.getElementById("hdk2").style.backgroundColor = 'var(--smooth)'
+//     document.getElementById("hdk3").style.backgroundColor = 'var(--smooth)'
+//     document.getElementById("hdk4").style.backgroundColor = 'var(--smooth)'
+//     document.getElementById("hdk5").style.backgroundColor = 'var(--smooth)'
+//     document.getElementById("hdk6").style.backgroundColor = 'var(--smooth)'
+
+// }
+
+// function unhover(){
+//     document.getElementById("hdk1").style.backgroundColor = '#fd7bd6'
+//     document.getElementById("hdk2").style.backgroundColor = '#fd7bd6'
+//     document.getElementById("hdk3").style.backgroundColor = '#fd7bd6'
+//     document.getElementById("hdk4").style.backgroundColor = '#fd7bd6'
+//     document.getElementById("hdk5").style.backgroundColor = '#fd7bd6'
+//     document.getElementById("hdk6").style.backgroundColor = '#fd7bd6'
+
+// }
